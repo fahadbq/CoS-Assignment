28d4f6a750e53527d5c6a0850f25b1c0e3cad7ec
fahad <fahad.baig@costrategix.com>
2022-06-10 19:30:03 +0530

    installed router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App"> Hi 
+    <div className="App">
+      <NavBar />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const NavBar = (props) => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
